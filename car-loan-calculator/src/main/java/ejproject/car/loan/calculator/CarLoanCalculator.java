f925775ff46a91c0a65eebccfc8173d69287fbdd
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-03-01 23:32:37 +0800

    add user input for car make model

--- car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
+++ car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class CarLoanCalculator {
 
     public static void main(String[] args) {
-       
+        Scanner carMakeModel = new Scanner(System.in);
+        System.out.println("Car Make & Model: ");
+        String car = carMakeModel.nextLine();
+        
         
     }
 }
