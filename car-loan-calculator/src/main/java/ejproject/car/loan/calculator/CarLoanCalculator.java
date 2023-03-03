00d5a2ef032f2114f92903701b06aeb765e7bdb5
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-03-03 23:33:16 +0800

    add formula calculation

--- car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
+++ car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
@@ -20,5 +20,7 @@ public class CarLoanCalculator {
         Scanner paymentTerms = new Scanner(System.in);
         System.out.print("Payment Terms in Months (12, 24, 36, 48, 60): ");
         int terms = paymentTerms.nextIntI();
+        
+        double calculation = ((price-dp) / terms);
     }
 }
