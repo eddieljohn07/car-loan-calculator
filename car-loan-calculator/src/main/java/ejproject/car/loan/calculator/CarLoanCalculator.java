939eb65347075a227ac3590ff837c9fe317d0c80
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-03-17 23:18:23 +0800

    remove extra

--- car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
+++ car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
@@ -25,13 +25,13 @@ public class CarLoanCalculator {
         double secondCalculation = ((price - dp) / terms);
 
         if (terms == 12) {
-            System.out.println((("Php ) + ((calculation * 0.515) + secondCalculation)) + ((" is your monthly amortization for 12 months or 1 year for your ") + car));
+            System.out.println((("Php ) + ((calculation * 0.515) + secondCalculation)) + (( is your monthly amortization for 12 months or 1 year for your ") + car));
         }elif (terms == 24) {
-            System.out.println((("Php ) + ((calculation * 0.1019) + secondCalculation)) + ((" is your monthly amortization for 24 months or 2 years for your ") + car));
+            System.out.println((("Php ) + ((calculation * 0.1019) + secondCalculation)) + (( is your monthly amortization for 24 months or 2 years for your ") + car));
         }elif (terms == 36) {
-            System.out.println((("Php ) + ((calculation * 0.1563) + secondCalculation)) + ((" is your monthly amortization for 36 months or 3 years for your ") + car));
+            System.out.println((("Php ) + ((calculation * 0.1563) + secondCalculation)) + (( is your monthly amortization for 36 months or 3 years for your ") + car));
         }elif (terms == 48) {
-            System.out.println((("Php ) + ((calculation * 0.218) + secondCalculation)) + ((" is your monthly amortization for 48 months or 4 years for your ") + car));
+            System.out.println((("Php ) + ((calculation * 0.218) + secondCalculation)) + (( is your monthly amortization for 48 months or 4 years for your ") + car));
         }
    }
 }
