2dc20d93ccee26cdb9ef170cc089f64f77fc7a55
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-03-17 23:33:18 +0800

    add welcome

--- car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
+++ car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CarLoanCalculator {
 
     public static void main(String[] args) {
+        System.out.println("GET THE COMPUTATION OF YOUR DREAM CAR LOAN!");
         Scanner carMakeModel = new Scanner(System.in);
         System.out.print("Car Make & Model: ");
         String car = carMakeModel.nextLine();
