50a4bcf8f1cac6b0ac0c591cb17d5c5c3adb06cc
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-03-27 00:07:46 +0800

    add double input

--- car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
+++ car-loan-calculator/src/main/java/ejproject/car/loan/calculator/CarLoanCalculator.java
@@ -19,7 +19,7 @@ public class CarLoanCalculator {
         
      
         System.out.print("DownPayment: Php ");
-        double dp = downPayment.nextDouble();
+        double dp = doubleInput.nextDouble();
         
         Scanner paymentTerms = new Scanner(System.in);
         System.out.print("Payment Terms in Months (12, 24, 36, 48, 60): ");
